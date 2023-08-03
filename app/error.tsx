4e8f8cbe1ca5b5f4bc14362c7b2b10a527c51c2d
Nanocirs <nanocirs@gmail.com>
2023-08-03 12:13:02 +0200

    Fixed error element not showing in production.

--- app/error.tsx
+++ app/error.tsx
@@ -1,16 +1,13 @@
 'use client'
-export default function Error({error} : { error: Error & { digest? : string} }) {
-
-    if (error.message === 'Cannot connect to database.') {
-        return (
-            <>
-                <div>
-                    <h2>Could not connect to the database. Please, try again later.</h2>
-                </div>
-                <div>
-                    <button className="btn tryagain" onClick={() => window.location.reload()}>Try again</button>
-                </div>
-            </>
-        );
-    }
+export default function Error() {
+    return (
+        <>
+            <div>
+                <h2>Could not connect to the database. Please, try again later.</h2>
+            </div>
+            <div>
+                <button className="btn tryagain" onClick={() => window.location.reload()}>Try again</button>
+            </div>
+        </>
+    );
 }
